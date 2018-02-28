6cbc815722562840d8d10f08c713e27b530f04d9
maandeep98 <maandeep998@gmail.com>
2018-02-28 21:17:25 +0530

    comments added

--- Movie_Project/media.py
+++ Movie_Project/media.py
@@ -1,10 +1,12 @@
 import webbrowser
 
+#creating a class
 class Movie:
     def __init__(self , movie_title,movie_about,movie_poster,movie_trailor):
         self.title = movie_title
         self.storyline = movie_about
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailor
+    #play trailer
     def show_trailor(self):
         webbrowser.open(self.trailer_youtube_url)
